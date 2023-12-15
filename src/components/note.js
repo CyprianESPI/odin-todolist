2283958803dd70444357880dceafd13147fb93c6
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-15 11:57:40 +0100

    Add Note component

---
+++ src/components/note.js
@@ -0,0 +1,11 @@
+// components/note.js
+class Note {
+    constructor(title, content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // Other methods or properties related to Note class
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import styles from "./styles.css";
+import Note from './components/note.js';
 
-console.log("hello");
-
-alert("bye");
+// Now you can create an instance of the Note class
+const myNote = new Note('Title', 'Content');
+console.log(myNote);
