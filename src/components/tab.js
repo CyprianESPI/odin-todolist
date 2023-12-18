750573031f35341097646f08353e7ece2f5678f1
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-18 22:57:36 +0100

    Change style of selected tab

--- src/components/tab.js
+++ src/components/tab.js
@@ -2,30 +2,36 @@ class Tab {
     static TABS = [];
     static active = "tabActive";
     static inactive = "tabInactive";
+    static btnActive = "btnActive";
+    static btnInactive = "btnInactive";
 
     constructor(name, content) {
         this.name = name;
         this.content = content;
         this.content.className += Tab.active;
+        this.button = null;
     }
 
     CreateTabButton() {
         const elem = document.createElement("button");
-        elem.className = "tabButton";
+        elem.className = "tabButton " + Tab.btnInactive;
         elem.innerText = this.name;
 
         elem.addEventListener("click", () => {
             this.SetActive();
         });
 
+        this.button = elem;
         return elem;
     }
 
     SetActive() {
         Tab.TABS.forEach((tab) => {
             tab.content.className = tab.content.className.replace(Tab.active, Tab.inactive);
+            tab.button.className = tab.button.className.replace(Tab.btnActive, Tab.btnInactive);
         });
         this.content.className = this.content.className.replace(Tab.inactive, Tab.active);
+        this.button.className = this.button.className.replace(Tab.btnInactive, Tab.btnActive);
     }
 }
 
